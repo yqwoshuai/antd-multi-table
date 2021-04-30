debbc6c64494ab41498743f7ab6797ff35289818
edz <>
2021-04-30 14:06:31 +0800

    初始化

---
+++ src/IndentLineTable/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function IndentLineTable() {
+  return <div></div>;
+}
---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import IndentLineTable from "./IndentLineTable";
+
+ReactDOM.render(<IndentLineTable />, document.getElementById("root"));
